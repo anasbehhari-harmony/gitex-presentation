7734707c414354e89994a84719fb936f1a176812
anasbehhari-harmony <a.behhari@harmony.ma>
2024-05-27 10:32:15 +0100

    update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 cd C:\Users\PC\gitex-presentation
 pm2 start deploy.json
-pm2 -f save
-start msedge "http://localhost:3000"
+pm2 save -f
+timeout /t 5 /nobreak > NUL
+start "" "msedge.exe" "http://localhost:3000"
